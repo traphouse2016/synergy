fb5e168bc3f64b06d5c0657171a929acfa863f13
traphouse2016 <traphouse2016@riseup.net>
2026-05-03 09:54:31 -0700

    Delete Synergy_1_0_telegram_chromium_only_fixed45_save_fix_persistent_profiles directory

--- Synergy_1_0_telegram_chromium_only_fixed45_save_fix_persistent_profiles/stop.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Stopping GV Bot...
-taskkill /f /im python.exe /fi "WINDOWTITLE eq GV Bot Backend" >nul 2>&1
-taskkill /f /im electron.exe >nul 2>&1
-taskkill /f /im Electron.exe >nul 2>&1
-echo Done.
-timeout /t 2 /nobreak >nul
